d805ceb90b2e9e56c8afb888385225b98632ca7e
jogyoulsam <winter001110@gmail.com>
2023-09-01 10:43:30 +0900

    2-15오류

--- src/main/java/com/sbs/sbb/question/QuestionService.java
+++ src/main/java/com/sbs/sbb/question/QuestionService.java
@@ -5,6 +5,7 @@ import com.sbs.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +16,7 @@ public class QuestionService {
     private final QuestionRepository questionRepository; // -> 이걸 삭제해버려서 실행이 안됐음
 
     public List<Question> getList() {
+
         return this.questionRepository.findAll(); // -> 이것도 또한 삭제를 같이 해버려서 실행이 안됐음
     }
 
@@ -25,5 +27,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
