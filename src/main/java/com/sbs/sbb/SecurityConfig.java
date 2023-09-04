e2580b14103fceb5a709040ce1e6b8126368d60e
jogyoulsam <winter001110@gmail.com>
2023-09-04 12:01:29 +0900

    3-5시ì큐리티 콘피그

--- src/main/java/com/sbs/sbb/SecurityConfig.java
+++ src/main/java/com/sbs/sbb/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -12,7 +13,13 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizaeHttpRequests) -> authorizaeHttpRequests.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+        http.authorizeHttpRequests((authorizaeHttpRequests) -> authorizaeHttpRequests.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        .csrf((csrf) -> csrf
+                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+        .headers((headers) -> headers
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+        ;
         return http.build();
 
     }
